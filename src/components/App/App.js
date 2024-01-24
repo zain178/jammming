b1c22952b4e77a3cb91e348a56589111ceec6095
Zain Naqvi <zainnaqvi0@hotmail.com>
2024-01-24 21:20:15 +1100

    app and search bar functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,20 +6,60 @@ import Playlist from '../Playlist/Playlist';
 
 function App() {
 
-  cosnt [searchResults, setSearchResults] = useState([])
-  cosnt [playlistName, setPlaylistName] = useState("New Playlist")
-  cosnt [playlistTracks, setplaylistTracks] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [playlistName, setPlaylistName] = useState("New Playlist")
+  const [playlistTracks, setplaylistTracks] = useState([])
 
   const search = useCallback((trem) => {
     Spotify.search(terrm).then(setSearchResults);
   }, []);
+
+  const addTrack = useCallback((track) => {
+    if(playlistTracks.some((savedTrack) => savedTrack.id === track.id))
+      return;
+
+    setplaylistTracks((prevTrack) => [...prevTrack, track]);  
+  }, [playlistTracks]
+  );
+
+  const removeTrack = useCallback((track) => {
+    setplaylistTracks((prevTracks) => 
+      prevTracks.filter((currentTrack) => currentTrack.id !== track.id) 
+    );
+  }, []);
+
+  const updatePlaylistName = useCallback((name) => {
+    setPlaylistName(name);
+  }, [])
+
+  const savePlatlist = useCallback(() => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    Spotify.savePlatlist(playlistName, trackUris).then(() => {
+      setPlaylistName("New Playlist");
+      setplaylistTracks([]);
+    });
+  }, [playlistName, playlistTracks]);
   
   return (
-    <div className="App">
-      <h1>Ja<span>mmm</span>ing</h1>
-      <SearchBar />
+    <div>
+      <h1>
+        Ja<span className='highlight'>mmm</span>ing
+      </h1>
+      <div className='App'>
+        <SearchBar onSearch={search} />
+        <div className='App-playlist'>
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist 
+            playlistName={playlistName}
+            playlistTracks={playlistTracks}
+            onNameCahnge={updatePlaylistName}
+            onRemove={removeTrack}
+            onSave={savePlatlist}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,14 +2,24 @@ import React, { useState, useCallback } from "react";
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-    
+    const [term, setTerm] = useState("")
+
+    const handleTermChanghe= useCallback((event) => {
+        setTerm(event.target.value);
+    }, [])
+
+    const search = useCallback(() => {
+        props.onSearch(term);
+    }, [props.onSearch, term]);
 
     return (
         <div className="SearchBar">
-            <input placeholder="Enter a song here"/>
-            <button>SEARCH</button>
+            <input placeholder="Enter A Song Title" onChange={handleTermChanghe} />
+            <button className="SearchButton" onClick={search}>
+                SEARCH
+            </button>
         </div>
     );
-} 
+};
 
 export default SearchBar;
